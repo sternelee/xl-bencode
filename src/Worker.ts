c0589bfebcccc5d4caebcd565a61f4ca4a98ef52
李兴广 <lixingguang@xunlei.com>
2021-12-16 17:13:49 +0800

    update

--- src/Worker.ts
+++ src/Worker.ts
@@ -4,7 +4,7 @@ import cryptoJs from 'crypto-js'
 
 export type CODE_TYPE = '32' | '40'
 
-function decode (buf: ArrayBuffer, codeType: CODE_TYPE = '32') {
+function decode (buf: ArrayBuffer, codeType: CODE_TYPE = '40') {
     const buffer = new Uint8Array(buf);
 		const { info } = Bencode.decode(buffer);
 		const encodeInfo = Bencode.encode(info);
@@ -14,7 +14,7 @@ function decode (buf: ArrayBuffer, codeType: CODE_TYPE = '32') {
       // TODO： 转成32位
       // 40位是十六进制字符串, 32位时base32字符串
     }
-    return hash.toUpperCase()
+    return hash
 }
 
 expose({
